9800309dd1f7779c42a2387a930745d166029fe1
aurorex <selva_beat@hotmail.com>
2018-03-14 10:01:44 -0500

    subiendo proyecto

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+const Header = () => {
+      <header>Hola soy Aurora</header>
+}
+
+const Section = () => {
+     <section>
+       <h2>Gustos</h2>
+       <ul>
+         <li>Programar</li>
+         <li>Actuar</li>
+         <li>Bailar</li>
+         
+       </ul>
+     </section>
+}
+const Footer = () => {
+     <footer>Copirigth</footer>
+}
 
 const App = () => (
-  <div>React and redux are cool</div>
+ <div>
+   <Header/>
+   <Section/>
+   <Footer/>
+ </div>
 )
 
 export default App
+
